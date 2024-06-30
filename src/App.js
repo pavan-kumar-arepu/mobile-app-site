3d1fc9c379dd85d4e0917388d3f372984906e6b3
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-06-30 16:16:58 +0200

    Updated AppJs and Packages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Tabs/Home/Home";
+import Contact from "./components/Tabs/Contact/Contact";
+
+import DummyScreen from "./components/Tabs/Dummy/DummyScreen";
+
+import ContributionScreen from "./components/Tabs/Contributions/ContributionScreen";
+import StoreApps from "./components/Tabs/StoreApps/StoreApps";
+import MyQuoteScreen from "./components/Tabs/MyQuotes/MyQuoteScreen";
+import LinkedinScreen from "./components/Tabs/Linkedin/LinkedinScreen";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<DummyScreen tabName="AboutMe" />} />
+          <Route
+            path="/contributions"
+            element={<ContributionScreen tabName="Contributions" />}
+          />
+          <Route
+            path="/linkedin"
+            element={<LinkedinScreen tabName="Linkedin" />}
+          />
+          <Route path="/apps" element={<StoreApps tabName="StoreApps" />} />
+          <Route
+            path="/quotes"
+            element={<MyQuoteScreen tabName="MyQuotes" />}
+          />
+          {/* Add more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
